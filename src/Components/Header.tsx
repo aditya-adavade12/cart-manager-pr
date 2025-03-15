19609a9d355d8f148cfbd2162e5d87a794477f19
Aditya Adavade <adityawork118@gmail.com>
2025-03-15 15:15:00 +0530

    Added header component

---
+++ src/Components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header:React.FC = () => {
+  return (
+    <div>
+      <div className='bg-black text-white text-xl w-full text-center p-2'>Cart Manager</div>
+    </div>
+  )
+}
+
+export default Header
