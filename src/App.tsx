7c6d3f99e2269067c1845f5a6104c99a7bf2b6fb
Aditya Adavade <adityawork118@gmail.com>
2025-03-21 12:01:49 +0530

    Updated app

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import Product from './components/Product';
+import Cart from './components/Cart';
 
 
   const App:React.FC = () => {
     return (
       <div>
        <Product />
+       <Cart />
       </div>
     )
   }
