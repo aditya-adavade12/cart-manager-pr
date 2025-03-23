cf63cbd20db248d31d1a2e58860613402c7cd032
Aditya Adavade <adityawork118@gmail.com>
2025-03-23 13:11:26 +0530

    Updated cart function

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -3,12 +3,12 @@ import React, { useEffect, useState } from 'react'
 const Product: React.FC = () => {
     // Product array of objects
     const productArray: any = [
-        { id: 1, name: "Spicy Chettinad Chicken", price: 3499, img: "https://cdn.shopify.com/s/files/1/0261/7960/0481/files/chicken_chettinand_made_from_non-veg_spices_480x480.jpg?v=1693671559" },
-        { id: 2, name: "Chicken 65", price: 9499, img: "https://blog.swiggy.com/wp-content/uploads/2024/09/Image-2_Rogan-Josh-1024x538.png" },
-        { id: 3, name: "Prawn Thokku", price: 8499, img: "https://cdn.shopify.com/s/files/1/0261/7960/0481/files/prawn_thokku_480x480.jpg?v=1693671877" },
-        { id: 4, name: "Egg Bhurji", price: 2499, img: "https://cdn.shopify.com/s/files/1/0261/7960/0481/files/spicy_egg_bhurji_480x480.jpg?v=1693672157" },
-        { id: 5, name: "Lamb Chops", price: 1499, img: "https://cdn.shopify.com/s/files/1/0261/7960/0481/files/lamb_chops_480x480.jpg?v=1693673499" },
-        { id: 6, name: "Butter Chicken", price: 2499, img: "https://cdn.shopify.com/s/files/1/0261/7960/0481/files/butter_chicken_masala_480x480.jpg?v=1693674488" }
+        { id: 1, name: "Spicy Chettinad Chicken", price: 3499, img: "https://cdn.shopify.com/s/files/1/0261/7960/0481/files/chicken_chettinand_made_from_non-veg_spices_480x480.jpg?v=1693671559", qty: 1 },
+        { id: 2, name: "Chicken 65", price: 9499, img: "https://blog.swiggy.com/wp-content/uploads/2024/09/Image-2_Rogan-Josh-1024x538.png", qty: 1 },
+        { id: 3, name: "Prawn Thokku", price: 8499, img: "https://cdn.shopify.com/s/files/1/0261/7960/0481/files/prawn_thokku_480x480.jpg?v=1693671877", qty: 1 },
+        { id: 4, name: "Egg Bhurji", price: 2499, img: "https://cdn.shopify.com/s/files/1/0261/7960/0481/files/spicy_egg_bhurji_480x480.jpg?v=1693672157", qty: 1 },
+        { id: 5, name: "Lamb Chops", price: 1499, img: "https://cdn.shopify.com/s/files/1/0261/7960/0481/files/lamb_chops_480x480.jpg?v=1693673499", qty: 1 },
+        { id: 6, name: "Butter Chicken", price: 2499, img: "https://cdn.shopify.com/s/files/1/0261/7960/0481/files/butter_chicken_masala_480x480.jpg?v=1693674488", qty: 1 }
     ]
     // Add to cart
     interface Cart {
@@ -16,21 +16,27 @@ const Product: React.FC = () => {
         name: string,
         img: any,
         price: number,
+        qty: number
     }
-    const [cart, setCart] = useState<Cart[]>([]);
+    const [cart, setCart] = useState<Cart[]>(() => {
+        return JSON.parse(localStorage.getItem("Cart") || "[]");
+    });
     // Syncing cart state with localstorage
+
     useEffect(() => {
-       localStorage.setItem("Cart", JSON.stringify(cart));    
+        localStorage.setItem("Cart", JSON.stringify(cart));    
     }, [cart]);
+
     const addtoCart = (item:any) => {
-        const getCart = JSON.parse(localStorage.getItem("Cart") || "[]");
-        console.log(getCart);
-        const getId = getCart.map((product:any) =>  product.id === item.id)
-        if (getId === "[]") {
-            console.log("not found");
-        } else {
-            setCart((prevCart) => [...prevCart, item]);
-        }
+        setCart((prevCart) => {
+            // Cart checking for product id
+            const updateCart = prevCart.map((product) =>
+                product.id === item.id ? {...product, qty: product.qty + 1} : product,
+            );
+            // Item exist
+            const itemExists = prevCart.some((product) => product.id === item.id)
+            return itemExists ? updateCart : [...prevCart, {...item, qty : 1}];
+        })
     }
     return (
         <div className='w-[50vw] mx-auto'>
